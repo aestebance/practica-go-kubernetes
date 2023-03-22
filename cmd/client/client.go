98e446519fd8f0c7035d91702a626c53fb1f4974
Alejandro Esteban Celdran <Alejandro.Esteban@ree.es>
2023-03-22 09:46:35 +0100

    added client

---
+++ cmd/client/client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aestebance/practica-go-kubernetes/lib/client"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+var (
+	sleepTime string = "1s"
+	serverURL string = "http://localhost:8000"
+)
+
+func main() {
+	setupClient := client.SetupClient()
+
+	if len(os.Getenv("SLEEP_TIME")) != 0 {
+		sleepTime = os.Getenv("SLEEP_TIME")
+	}
+
+	if len(os.Getenv("SERVER_URL")) != 0 {
+		serverURL = os.Getenv("SERVER_URL")
+	}
+
+	endpoint := os.Getenv("ENDPOINT")
+
+	for {
+		accessPoint := fmt.Sprint(serverURL, endpoint)
+		r, err := setupClient.Get(accessPoint)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}(r.Body)
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s\n", body)
+
+		sleepTimeDuration, _ := time.ParseDuration(sleepTime)
+		time.Sleep(sleepTimeDuration)
+	}
+}
